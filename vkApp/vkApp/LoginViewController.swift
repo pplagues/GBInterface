ba666191cb75be14b6806a6c5fb248fa77a99bd8
Mikhail Vasilev <iwheelbuy@gmail.com>
2021-04-08 13:40:34 +0700

    Keyboard constraints

--- vkApp/vkApp/LoginViewController.swift
+++ vkApp/vkApp/LoginViewController.swift
@@ -8,21 +8,44 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    
-    @IBOutlet var loginTextField: UITextField!
-    
-    @IBOutlet var passwordTextField: UITextField!
-    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+   @IBOutlet var loginTextField: UITextField!
+   @IBOutlet var passwordTextField: UITextField!
+   @IBOutlet var scrollView: UIScrollView!
+   @IBOutlet var bodyView: UIView!
+   @IBOutlet var constraintTop: NSLayoutConstraint!
+   @IBOutlet var constraintCenter: NSLayoutConstraint!
 
-    @IBAction func logInButton(_ sender: UIButton) {
-        print("Login: \(loginTextField.text ?? "")")
-        print("Password: \(passwordTextField.text ?? "")")
-    }
-    
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      NotificationCenter.default.addObserver(
+         self,
+         selector: #selector(keyboardWillShow(notification:)),
+         name: UIResponder.keyboardWillChangeFrameNotification,
+         object: nil
+      )
+   }
+
+   @IBAction func logInButton(_ sender: UIButton) {
+      print("Login: \(loginTextField.text ?? "")")
+      print("Password: \(passwordTextField.text ?? "")")
+   }
+
+   @objc func keyboardWillShow(notification: NSNotification) {
+      let frameValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
+      guard let keyboardFullHeight = frameValue?.cgRectValue.height else {
+         return
+      }
+      if keyboardFullHeight > 200 {
+         let safeArea = view.frame.height - scrollView.frame.origin.y - scrollView.frame.height
+         let keyboardOnScrollHeight = keyboardFullHeight - safeArea
+         constraintTop.constant = scrollView.frame.height - bodyView.frame.height - keyboardOnScrollHeight
+         constraintCenter.isActive = false
+      } else {
+         constraintCenter.isActive = true
+      }
+      UIView.animate(withDuration: 0.3) { [view] in
+         view?.layoutIfNeeded()
+      }
+   }
 }
-
